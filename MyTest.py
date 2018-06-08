72344b1a860a92cfc9f2ffc05dce748823676a16
wangmumu321 <33012726+wangmumu321@users.noreply.github.com>
2018-06-08 11:25:40 +0800

    Update MyTest.py
    Use dynamic programming instead of greedy algorithms.

--- MyTest.py
+++ MyTest.py
@@ -7,8 +7,8 @@ entries = []
 profit = []
 profit_ave = []
 sort_profit_ave = []
-#min_w = 1
-#min_h = 1
+min_w = 1
+min_h = 1
 
 def get_min():
     global min_h,min_w
@@ -44,25 +44,35 @@ def verify_data():
         return -2
     return 1
 
-def get_total_profit(w,h):
+def get_total_profit(w,h,i,flag,count):
+
     if w==0 or h ==0:
         #print("get profit : 0")
         return 0
-    '''
-    if w<min_w or h<min_h:
-        print("get profit : -1")
+
+    if count > max_dep:
         return -1
-    '''
-    for p_av in sort_profit_ave:
-        index = profit_ave.index(p_av)
-        w1 = entries[index][0]
-        h1 = entries[index][1]
+
+    for entry in entries:
+        #print("entry is :", entry)
+
+        index = entries.index(entry)
+        #print("index is ",index)
+        w1 = entry[0]
+        h1 = entry[1]
         if w>=w1 and h>=h1:
-            print("get profit : ",profit[index] )
-            print("w and h is :",w1,h1)
-            return profit[index] + get_total_profit(w-w1,h) + get_total_profit(w1, h-h1)
-    print("get profit : -1")
-    print("w and h is :", w, h)
+            if flag == 0 and index == i:
+                continue
+            profit_1 = profit[index] + get_total_profit(w-w1,h,index,1,count+1) + get_total_profit(w1, h-h1,index,1,count+1)
+            profit_2 = get_total_profit(w,h,index ,0,count+1)
+            max_profit =  max(profit_1, profit_2)
+            if max_profit == profit_1:
+                print("W and H is :",w,h )
+                print("get entry : ", entry)
+                print("get profit : ",  profit[index])
+            return max_profit
+    #print("get profit : -1")
+    #print("w and h is :", w, h)
     return -1
 
 
@@ -72,15 +82,16 @@ if __name__ == '__main__':
     print("metal_H is: " , metal_H)
     print("entries is: " , entries)
     print("profit is: " , profit)
-    print("profit_ave is: ", profit_ave)
+    #print("profit_ave is: ", profit_ave)
 
     if verify_data():
-        sort_profit_ave = sorted(profit_ave)
-        sort_profit_ave.reverse()
-        print("sort_profit_ave is: ",sort_profit_ave)
+        #sort_profit_ave = sorted(profit_ave)
+        #sort_profit_ave.reverse()
+        #print("sort_profit_ave is: ",sort_profit_ave)
 
-        #get_min()
-        #print("min_w is :", min_w)
-        #print("min_h is :", min_h)
+        get_min()
+        print("min_w is :", min_w)
+        print("min_h is :", min_h)
+        max_dep = metal_W*metal_H/(min_w*min_h)
 
-        print(get_total_profit(metal_W,metal_H))
+        print("total profit is : ",get_total_profit(metal_W,metal_H,-1,-1,0))
